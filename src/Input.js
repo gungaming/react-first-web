fd0a9bcc96d783e98410c1d3c9a9b9cd0421adc9
Voragun Supawong <voragun2543@gmail.com>
2021-01-01 21:03:48 +0700

    [ADD] Navbar & Input field

---
+++ src/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Input() {
+    return (
+        <div className="Input">
+            <div className="Input__header">Create post</div>
+            <input className="Input__field" type="text"/>
+        </div>
+    )
+}
+
+export default Input;
