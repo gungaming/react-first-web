a70a22f8802b8a4f6f18f68018d58ad53e7ba3c6
Voragun Supawong <voragun2543@gmail.com>
2021-01-01 23:19:38 +0700

    [ADD] Handling Event and Rendering
    Complete react crash course series

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Navbar from './Navbar'
 import Input from './Input'
+import Post from './Post'
 
 let id = 1;
 
@@ -15,10 +16,18 @@ function App() {
     id += 1;
   }
 
+  function deletePost (id) {
+    const updatedPost = posts.filter((post) => post.id !== id);
+    setPosts(updatedPost);
+  }
+
   return (
     <div className="App">
       <Navbar />
       <Input addPost={addPost} />
+      {posts.map((post) => (
+        <Post key={post.id} id={post.id} title={post.title} deletePost={deletePost}/>
+      ))}
     </div>
   );
 }
--- src/Input.js
+++ src/Input.js
@@ -4,10 +4,29 @@ import PropTypes from 'prop-types';
 function Input({ addPost }) {
     const [input, setInput] = useState('');
 
+    function onChange(event) {
+        setInput(event.target.value);
+    }
+
+    function onKeyDown(event) {
+        const title = event.target.value;
+
+        if (event.key === 'Enter' && title) {
+            addPost(title);
+            setInput('');
+        }
+    }
+
     return (
         <div className="Input">
             <div className="Input__header">Create post</div>
-            <input className="Input__field" type="text" value={input}/>
+            <input 
+                className="Input__field" 
+                type="text" 
+                value={input} 
+                onChange={onChange} 
+                onKeyDown={onKeyDown}
+            />
         </div>
     )
 }
